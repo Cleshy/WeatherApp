8faafe8886d4c60e50dae7d6e40bfcf764333663
Czipa Szilárd <szilard.czipa@gmail.com>
2025-06-21 11:00:19 +0200

    Add DayForecast component.

--- src/components/DayForecast.jsx
+++ src/components/DayForecast.jsx
@@ -1,7 +1,16 @@
+import DayForecastCard from "./DayForecastCard";
+
 function DayForecast() {
   return (
     <section className="day-forecast">
-      <h2>5-day forecast</h2>
+      <h2 className="day-forecast__title">5-day forecast</h2>
+      <div className="day-forecast-card-container">
+        <DayForecastCard />
+        <DayForecastCard />
+        <DayForecastCard />
+        <DayForecastCard />
+        <DayForecastCard />
+      </div>
     </section>
   );
 }
--- src/components/DayForecastCard.jsx
+++ src/components/DayForecastCard.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import icon from "../icons/01d.png";
 
 function DayForecastCard() {
-  return <div className="day-forecast-card"></div>;
+  return (
+    <div className="day-forecast-card">
+      <p className="day-forecast__day">Today</p>
+      <img className="day-forecast__icon" src={icon} alt="" />
+      <div className="day-forecast__temp">
+        <p className="day-forecast__temp-text">-1°</p>
+        <div className="day-forecast__temp-bar"></div>
+        <p className="day-forecast__temp-text">3°</p>
+      </div>
+    </div>
+  );
 }
 
 export default DayForecastCard;
